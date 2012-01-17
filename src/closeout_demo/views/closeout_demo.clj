ef36ced6b0deb7ea49a1ebc7c1763376d946febc
Dave Sann <davesann.p@gmail.com>
2012-01-17 20:42:32 +1100

    added google +1 and facebook like

--- src/closeout_demo/views/closeout_demo.clj
+++ src/closeout_demo/views/closeout_demo.clj
@@ -15,6 +15,53 @@
 (def clojurescript [:a {:href "https://github.com/clojure/clojurescript/wiki"} "ClojureScript"])
 (def closeout      [:a.closeout {:href "https://github.com/davesann/closeout"} "Closeout"])
 (def knockout      [:a {:href "http://knockoutjs.com/"} "Knockoutjs"])
+(def google-analytics
+   [:script {:type "text/javascript"}
+    "var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28393777-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();"])
+
+(def google+1-html
+  [:div {:class "g-plusone" 
+         :data-size "small" 
+         :data-annotation "inline" 
+         :data-href="http://closeout-demo.herokuapp.com/"}])
+
+(def google+1-script
+  [:script {:type "text/javascript"}
+   "window.___gcfg = {lang: 'en-GB'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();"])
+
+(def facebook-like-script
+  (list
+    [:div.fb-root]
+    [:script {:type "text/javascript"}
+     "(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = '//connect.facebook.net/en_GB/all.js#xfbml=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));"]))
+
+(def facebook-like-html
+  [:div {:class "fb-like"
+         :data-href "http://closeout-demo.herokuapp.com/" 
+         :data-send "true" 
+         :data-width "450" 
+         :data-show-faces "true"}
+   ])
 
 (defn app-choice [app-id]
   [:div.app-choice
@@ -33,6 +80,7 @@
   [app-id]
   [:body#body
    (if app-id {:data-app-choice app-id} {})
+   facebook-like-script
    [:div.intro
     [:div.title "ClojureScript applications with Closeout"]
     [:div.desc
@@ -68,23 +116,14 @@
        [:td "Clojurescript source"]
        [:td [:a {:href "https://github.com/davesann/closeout-demo-cljs"} 
              "https://github.com/davesann/closeout-demo-cljs"]]]
+      [:tr [:td] [:td "&nbsp;"]]
+      [:tr [:td] [:td google+1-html]]
+      [:tr [:td] [:td facebook-like-html]]
       ]
      ]
     ]
    ]
   )
-
-(def google-analytics
-   [:script {:type "text/javascript"}
-    "var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-28393777-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();"])
   
 (defn page 
   ([] (page nil))
@@ -104,6 +143,7 @@
          
          :else
          (hph/include-css (str "/css/clock.css")))
+       google+1-script
        google-analytics
        ]
       (body-template app-id)
