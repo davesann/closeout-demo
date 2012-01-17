2983eb79c6add8253e626b60052bc947b9cd46bd
Dave Sann <davesann.p@gmail.com>
2012-01-17 21:32:47 +1100

    remove facebook blurb

--- src/closeout_demo/views/closeout_demo.clj
+++ src/closeout_demo/views/closeout_demo.clj
@@ -59,7 +59,7 @@
   [:div {:class "fb-like"
          :data-href "http://closeout-demo.herokuapp.com/" 
          :data-send "true"
-         ;:data-layout "button_count"
+         :data-layout "button_count"
          :data-width "450" 
          :data-show-faces "true"}
    ])
