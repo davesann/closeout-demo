d3e2b38a7555e31efbd0e60d088021b8f84356df
Dave Sann <davesann.p@gmail.com>
2012-01-17 14:53:33 +1100

    Added procfile and google analytics

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: lein run -m closeout-demo.server $PORT
--- src/closeout_demo/views/closeout_demo.clj
+++ src/closeout_demo/views/closeout_demo.clj
@@ -55,6 +55,18 @@
      [:li "The css for this site expects a modern browser."]
    ]]]
    )
+
+(def google-analytics
+   [:script {:type "text/javascript"}
+    "var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28393777-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();"])
   
 (defn page 
   ([] (page nil))
@@ -74,6 +86,7 @@
          
          :else
          (hph/include-css (str "/css/clock.css")))
+       google-analytics
        ]
       (body-template app-id)
       (hph/include-js "/js/bootstrap.js")
