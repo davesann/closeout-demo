091772dc880040f4eae872091e8259d73c5cdc61
Dave Sann <davesann.p@gmail.com>
2012-01-17 21:15:33 +1100

    and...twitter

--- src/closeout_demo/views/closeout_demo.clj
+++ src/closeout_demo/views/closeout_demo.clj
@@ -58,10 +58,21 @@
 (def facebook-like-html
   [:div {:class "fb-like"
          :data-href "http://closeout-demo.herokuapp.com/" 
-         :data-send "true" 
+         :data-send "true"
+         ;:data-layout "button_count"
          :data-width "450" 
          :data-show-faces "true"}
    ])
+
+(def twitter-html
+  (list
+    [:a {:href "https://twitter.com/share"
+         :class "twitter-share-button"
+         :data-url "http://closeout-demo.herokuapp.com/" 
+         :data-text "Closeout - clojurescript ui-binding" 
+         :data-hashtags="clojurescript, clojure, closeout"} "Tweet"]
+    [:script "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');"]
+    ))
 
 (defn app-choice [app-id]
   [:div.app-choice
@@ -119,6 +130,7 @@
       [:tr [:td] [:td "&nbsp;"]]
       [:tr [:td] [:td google+1-html]]
       [:tr [:td] [:td facebook-like-html]]
+      [:tr [:td] [:td twitter-html]]
       ]
      ]
     ]
